66b966dd39499f9b885fbb7e40abdbf29f4ab4b4
Karinecasant <77842351+Karinecasant@users.noreply.github.com>
2021-02-13 03:59:27 -0300

    Add files via upload

---
+++ Aula1/Ex2.sql
@@ -0,0 +1,34 @@
+create database db_ecommerce;
+
+use db_ecommerce;
+
+create table tb_produtos(
+id bigint(8) auto_increment,
+produto varchar(100) not null,
+tamanho varchar(100) not null,
+cor varchar(100) not null,
+fabricante varchar(100) not null,
+tecido varchar(100) not null,
+genero varchar(100) not null, 
+valor double not null,
+primary key (id)
+);
+
+insert into tb_produtos(produto, tamanho, cor, fabricante, tecido, genero, valor) values
+("camiseta", "P", "vermelho", "Sempre Bela", "algodao", "feminino", 100),
+("calca", "G", "amarelo", "Gold Style", "linho", "masculino", 200),
+("casaco", "P", "azul", "Belo Azul", "algodao", "feminino", 300),
+("vestido", "M", "laranja", "Western Fashion", "linho", "masculino", 400),
+("sueter", "G", "roxo", "Duds", "algodao", "feminino", 500),
+("luvas", "P", "vinho", "Fashion Star", "linho", "masculino", 600),
+("jaqueta", "M", "azul", "Woman Village", "algodao", "feminino", 700),
+("short", "G", "preto", "MeninBlue", "linho", "masculino", 800);
+
+-- Faça um select que retorne os produtos com o valor maior do que 500.
+select * from tb_produtos  where valor > 500
+
+-- Faça um select que retorne os produtos com o valor menor do que 500.
+select * from tb_produtos  where valor < 500
+-- Ao término atualize um dado desta tabela através de uma query de atualização.
+update tb_produtos set valor = 150 where id = 1;
+
